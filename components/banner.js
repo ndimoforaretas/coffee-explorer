f397299f0a477c246b70d11f2c2cafe350b2dae6
ndimoforaretas <ndimaret@gmail.com>
2022-11-05 11:33:28 +0100

    configure home screen

---
+++ components/banner.js
@@ -0,0 +1,29 @@
+import styles from './banner.module.css';
+
+const Banner = (props) => {
+	return (
+		/*	<div>
+   
+			<h1>Kaffee Erforscher</h1>
+			<p>Entdecken Sie Ihre lokalen Cafés!</p>
+			<button>Läden in der Nähe ansehen</button>
+      
+		</div> */
+
+		<div className={styles.container}>
+			<h1 className={styles.title}>
+				<span className={styles.title1}>Coffee</span>
+				<span className={styles.title2}> Explorer</span>
+			</h1>
+			<p className={styles.subTitle}>Discover your local coffee shops!</p>
+			<button
+				className={styles.button}
+				onClick={props.handleOnClick}
+			>
+				{props.buttonText}
+			</button>
+		</div>
+	);
+};
+
+export default Banner;
