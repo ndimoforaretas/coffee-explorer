da543b18a1eee640d691962c1d10655418afd48b
ndimoforaretas <ndimaret@gmail.com>
2022-11-05 15:31:11 +0100

    add home style

---
+++ pages/coffee-store/[id].js
@@ -0,0 +1,16 @@
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+const CoffeeStore = () => {
+	const router = useRouter();
+	return (
+		<div>
+			Coffee Store Page {router.query.id}
+			<Link href='/'>
+				<a>Back to home</a>
+			</Link>
+		</div>
+	);
+};
+
+export default CoffeeStore;
