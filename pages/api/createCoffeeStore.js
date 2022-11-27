7fec3da3ef5b2eb522fcbb1df3fb6e06b352c91d
ndimoforaretas <ndimaret@gmail.com>
2022-11-27 18:16:36 +0100

    refactor API and handle error scenarios

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,14 +1,6 @@
-const Airtable = require('airtable');
-const base = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY }).base(
-	process.env.AIRTABLE_BASE_KEY
-);
-
-const table = base('coffee-stores');
-
-console.log({ table });
+import { table, getMinifiedRecords } from '../../lib/airtable';
 
 const createCoffeeStore = async (req, res) => {
-	console.log({ req });
 	if (req.method === 'POST') {
 		const { id, name, region, address, imgUrl, voting } = req.body;
 
@@ -21,14 +13,9 @@ const createCoffeeStore = async (req, res) => {
 					})
 					.firstPage();
 
-				console.log({ findCoffeeStoreRecords });
-
 				if (findCoffeeStoreRecords.length !== 0) {
-					const records = findCoffeeStoreRecords.map((record) => {
-						return {
-							...record.fields,
-						};
-					});
+					const records = getMinifiedRecords(findCoffeeStoreRecords);
+
 					res.json(records);
 				} else {
 					// create a record
@@ -46,11 +33,7 @@ const createCoffeeStore = async (req, res) => {
 							},
 						]);
 
-						const records = createRecords.map((record) => {
-							return {
-								...record.fields,
-							};
-						});
+						const records = getMinifiedRecords(createRecords);
 
 						res.json({ records });
 					} else {
