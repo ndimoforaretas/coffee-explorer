11dd7a7161df0771fe29cfda1c67a3c4407f38ad
ndimoforaretas <ndimaret@gmail.com>
2022-11-28 12:10:52 +0100

    refactor create coffee store by id API

--- pages/api/createCoffeeStore.js
+++ pages/api/createCoffeeStore.js
@@ -1,4 +1,8 @@
-import { table, getMinifiedRecords } from '../../lib/airtable';
+import {
+	table,
+	getMinifiedRecords,
+	findRecordByFilter,
+} from '../../lib/airtable';
 
 const createCoffeeStore = async (req, res) => {
 	if (req.method === 'POST') {
@@ -7,14 +11,10 @@ const createCoffeeStore = async (req, res) => {
 		try {
 			// finding a record
 			if (id) {
-				const findCoffeeStoreRecords = await table
-					.select({
-						filterByFormula: `id='${id}'`,
-					})
-					.firstPage();
+				const records = await findRecordByFilter(id);
 
-				if (findCoffeeStoreRecords.length !== 0) {
-					const records = getMinifiedRecords(findCoffeeStoreRecords);
+				if (records.length !== 0) {
+					res.json(records);
 
 					res.json(records);
 				} else {
